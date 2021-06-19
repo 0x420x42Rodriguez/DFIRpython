60c03e763aeadd8eb201aeda13003638d805a10e
ChrisM <medic9r1@gmail.com>
2021-06-19 15:51:53 -0500

    Update ptr_lookup.py

--- ptr_lookup.py
+++ ptr_lookup.py
@@ -1,6 +1,14 @@
+"""
+Pass a list of IP addresses that need to be resolved. Outputs a lists of the
+domains associated with the IP. Any address that does not resolve will be listed
+at the end of the output file.
+
+Replace the file paths to match your desired input/output.
+"""
+
 import socket
 
-with open(r"C:\Users\menne\Desktop\suspiciousIP.txt", "r") as input:
+with open(r"C:\Users\useraccount\Desktop\suspiciousIP.txt", "r") as input:
     ptr_lookup = []
     ip_address = input.readlines()
     converted_list = [] #Remove unwanted characters from original list
@@ -15,7 +23,7 @@ with open(r"C:\Users\menne\Desktop\suspiciousIP.txt", "r") as input:
         except socket.herror:
             error_log.append(f"Error, the IP address {i} did not resolve to a domain")
         
-    with open(r"C:\Users\menne\Desktop\resolvedIP.txt", "w+") as output:
+    with open(r"C:\Users\useraccount\Desktop\resolvedIP.txt", "w+") as output:
         for item in ptr_lookup:
             line = " ".join(str(x) for x in item)
             output.write(line + "\n")
